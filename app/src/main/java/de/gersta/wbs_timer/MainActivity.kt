0089ae2b02ef2fb8e59e3142addeba73ae7e328e
Gerrit Stapper <gerrit.stapper@accenture.com>
2021-08-20 08:23:11 +0200

    Switch between WBS elements
    Its now possible to press the button of another
    WBS to start its time and jump between each other without
    dropping seconds.
    
    Known bug: Clicking the same WBS twice in a row will reset it to 0.

--- app/src/main/java/de/gersta/wbs_timer/MainActivity.kt
+++ app/src/main/java/de/gersta/wbs_timer/MainActivity.kt
@@ -1,42 +1,74 @@
 package de.gersta.wbs_timer
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.Chronometer
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
-    var isChronoRunning = false
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    object ChronoConstants {
+        const val INITIAL_VALUE: Long = 0
+    }
+
     var stoppedTimer: Long = 0
+    var chronoPerType: HashMap<String, Long> = HashMap()
+
+    var currentButtonText = "NONE"
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val sport = findViewById<Button>(R.id.sport)
+        val gaming = findViewById<Button>(R.id.gaming)
 
-        val chronometer: Chronometer = findViewById(R.id.timer)
-        val startStopButton: Button = findViewById(R.id.start_stop_button)
+        sport.setOnClickListener(this)
+        gaming.setOnClickListener(this)
 
-        startStopButton.setOnClickListener {
-            if ( isChronoRunning ) {
-                chronometer.stop()
-                stoppedTimer = SystemClock.elapsedRealtime() - chronometer.base
-                Log.d("Stopped chronometer", "Elapsed time %d".format(stoppedTimer))
-
-                isChronoRunning = false
-                startStopButton.setText("Continue")
-            } else {
-                val start = SystemClock.elapsedRealtime() - stoppedTimer
-                Log.d("Starting chronometer", "Starting time %d".format(start))
-                chronometer.base = start
-                chronometer.start()
-
-                isChronoRunning = true
-                startStopButton.setText("Stop")
-            }
+        val stopButton = findViewById<Button>(R.id.stop_button)
+        stopButton.setOnClickListener {
+            val chronometer: Chronometer = findViewById(R.id.timer)
+            chronometer.stop()
+            chronoPerType.put(currentButtonText, SystemClock.elapsedRealtime() - chronometer.base)
+            Log.d("Stopped chronometer", "Elapsed time of %s: %d".format(currentButtonText, stoppedTimer))
         }
+    }
+
+    @SuppressLint("NewApi")
+    override fun onClick(view: View) {
+        val currentWbs = findViewById<TextView>(R.id.current_wbs)
+        Log.d("onClick", "Current WBS is %s".format(currentWbs.text))
+        val chronometer: Chronometer = findViewById(R.id.timer)
+
+        val clickedButton = findViewById<Button>(view.id)
+        currentWbs.text = clickedButton.text as String
+        Log.d("onClick", "Setting WBS to %s".format(currentWbs.text))
+
+        chronoPerType.putIfAbsent(clickedButton.text.toString(), ChronoConstants.INITIAL_VALUE)
+
+        if ( isDifferentWbsClicked(clickedButton) ) {
+            chronometer.stop()
+            chronoPerType.put(currentButtonText, SystemClock.elapsedRealtime() - chronometer.base)
+            Log.d("Stopped chronometer", "Elapsed time of %s: %d".format(currentButtonText, stoppedTimer))
+
+            currentButtonText = clickedButton.text as String
+        }
+
+
+        val start = SystemClock.elapsedRealtime() - chronoPerType.getOrElse(currentButtonText, { ChronoConstants.INITIAL_VALUE })
+        Log.d("Starting chronometer", "Starting time for %s: %d".format(currentButtonText, start))
+        chronometer.base = start
+        chronometer.start()
+    }
+
+
+    fun isDifferentWbsClicked(clickedButton: Button): Boolean {
+        return currentButtonText != clickedButton.text
     }
 }
